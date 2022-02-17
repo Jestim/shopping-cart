9572be2e4abafe91fb551a647d9ae72223da7f90
Jestim <jesper.os.thiman@gmail.com>
2022-02-18 00:07:13 +0100

    Fixed bug where app crashed when changing quntity on items in cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import { cloneDeep, forEach } from 'lodash';
+import { useEffect, useState } from 'react';
+import { cloneDeep } from 'lodash';
 import Header from './components/header/Header';
 import Home from './components/Home/Home';
 import Shop from './components/Shop/Shop';
@@ -10,7 +10,13 @@ import './app.css';
 function App() {
   const [cartItems, setCartItems] = useState([]);
 
+  // useEffect(() => {
+  //   console.log(cartItems);
+  // }, [cartItems]);
+
   const handleAddToCart = (product) => {
+    console.log('handleAddToCart called');
+
     const cartItemsUpdated = cloneDeep(cartItems);
 
     for (let i = 0; i < cartItemsUpdated.length; i += 1) {
@@ -23,16 +29,73 @@ function App() {
 
     const addedProduct = cloneDeep(product);
     addedProduct.quantity = 1;
-
     setCartItems([...cartItems, addedProduct]);
   };
 
+  const handleIncreseItemQuantity = (product) => {
+    console.log('handleIncreseItemQuantity called');
+
+    const cartItemsUpdated = cloneDeep(cartItems);
+
+    for (let i = 0; i < cartItemsUpdated.length; i += 1) {
+      if (cartItemsUpdated[i].id === product.id) {
+        cartItemsUpdated[i].quantity += 1;
+        setCartItems(cartItemsUpdated);
+        return;
+      }
+    }
+  };
+
+  const handleDecreseItemQuantity = (product) => {
+    console.log('handleDecreseItemQuantity called');
+
+    if (product.quantity === 1) {
+      return;
+    }
+
+    const cartItemsUpdated = cloneDeep(cartItems);
+
+    for (let i = 0; i < cartItemsUpdated.length; i += 1) {
+      if (cartItemsUpdated[i].id === product.id) {
+        cartItemsUpdated[i].quantity -= 1;
+        setCartItems(cartItemsUpdated);
+        return;
+      }
+    }
+  };
+
+  const handleRemoveItem = (product) => {
+    console.log('handleRemoveItem called');
+
+    const cartItemsUpdated = cloneDeep(cartItems);
+
+    for (let i = 0; i < cartItemsUpdated.length; i += 1) {
+      if (cartItemsUpdated[i].id === product.id) {
+        cartItemsUpdated.splice(i, 1);
+        setCartItems(cartItemsUpdated);
+        return;
+      }
+    }
+  };
+
   const getNumberOfCartItems = () => {
+    console.log('getNumberOfCartItems called');
+
     let cartItemsLength = 0;
     cartItems.forEach((item) => {
       cartItemsLength += item.quantity;
     });
     return cartItemsLength;
+  };
+
+  const getTotalPrice = () => {
+    console.log('getTotalPrice called');
+
+    let totalPrice = 0;
+    cartItems.forEach((item) => {
+      totalPrice += item.price * item.quantity;
+    });
+    return totalPrice;
   };
 
   return (
@@ -45,7 +108,18 @@ function App() {
             path="/shop"
             element={<Shop handleAddToCart={handleAddToCart} />}
           />
-          <Route path="/cart" element={<Cart cartItems={cartItems} />} />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                cartItems={cartItems}
+                handleIncreseItemQuantity={handleIncreseItemQuantity}
+                handleDecreseItemQuantity={handleDecreseItemQuantity}
+                handleRemoveItem={handleRemoveItem}
+                getTotalPrice={getTotalPrice}
+              />
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,20 @@ import './cart.css';
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai';
 import { HiOutlineTrash } from 'react-icons/hi';
 
-function Cart({ cartItems }) {
+function Cart({
+  cartItems,
+  handleIncreseItemQuantity,
+  handleDecreseItemQuantity,
+  handleRemoveItem,
+  getTotalPrice,
+}) {
   return (
     <div className="cart">
-      <h1 className="cart-header">In your shopping cart</h1>
+      {cartItems.length !== 0 ? (
+        <h1 className="cart-header">In your shopping cart</h1>
+      ) : (
+        <h1 className="cart-header">Your cart is empty</h1>
+      )}
       <div className="cart-items">
         {cartItems.map((item) => (
           <div className="cart-item-card" key={item.id}>
@@ -18,22 +28,39 @@ function Cart({ cartItems }) {
                 <div className="item-quantity-container">
                   <p className="item-quantity">{`Quantity: ${item.quantity}`}</p>
                   <div className="change-quantity-buttons">
-                    <button className="change-quantity-button" type="button">
+                    <button
+                      className="change-quantity-button"
+                      onClick={() => handleDecreseItemQuantity(item)}
+                      type="button"
+                    >
                       <AiOutlineMinusCircle />
                     </button>
-                    <button className="change-quantity-button" type="button">
+                    <button
+                      className="change-quantity-button"
+                      onClick={() => handleIncreseItemQuantity(item)}
+                      type="button"
+                    >
                       <AiOutlinePlusCircle />
                     </button>
                   </div>
                 </div>
                 <p className="item-price">{`$${item.price * item.quantity}`}</p>
-                <button className="remove-item-button" type="button">
+                <button
+                  className="remove-item-button"
+                  onClick={() => handleRemoveItem(item)}
+                  type="button"
+                >
                   <HiOutlineTrash />
                 </button>
               </div>
             </div>
           </div>
         ))}
+      </div>
+      <div className="total-price-container">
+        {cartItems.length !== 0 ? (
+          <p>{`Total price: $${getTotalPrice()}`}</p>
+        ) : null}
       </div>
     </div>
   );
